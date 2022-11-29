98d52a25a9987ddb8d108d0ba85c71aa141ca047
Nino <35476@ma-web.nl>
2022-11-29 11:59:37 +0100

    Resize + deur

--- canvashuis/app.js
+++ canvashuis/app.js
@@ -21,53 +21,76 @@ class App
         
         g.beginPath();
         g.fillStyle = "red"
-        g.moveTo(300,100);
-        g.lineTo(200,300);
-        g.lineTo(600,400);
-        g.lineTo(700,200);
-        g.lineTo(300,100);
+        g.moveTo(30,10);
+        g.lineTo(20,30);
+        g.lineTo(60,40);
+        g.lineTo(70,20);
+        g.lineTo(30,10);
         g.closePath();
         g.stroke();
         g.fill();
         
         g.beginPath();
         g.fillStyle = "lightgrey"
-        g.moveTo(200,300);
-        g.lineTo(200,500);
-        g.lineTo(600,600);
-        g.lineTo(600,400);
+        g.moveTo(20,30);
+        g.lineTo(20,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
         g.closePath();
         g.stroke();
         g.fill();
 
         g.beginPath();
         g.fillStyle = "grey"
-        g.moveTo(600,400);
-        g.lineTo(600,600);
-        g.lineTo(800,500);
-        g.lineTo(800,300);
+        g.moveTo(60,40);
+        g.lineTo(60,60);
+        g.lineTo(80,50);
+        g.lineTo(80,30);
         g.closePath();
         g.stroke();
         g.fill();
 
         g.beginPath();
         g.fillStyle = "grey"
-        g.moveTo(600,400);
-        g.lineTo(700,200);
-        g.lineTo(800,300);
+        g.moveTo(60,40);
+        g.lineTo(70,20);
+        g.lineTo(80,30);
         g.closePath();
         g.stroke();
         g.fill();
 
         g.beginPath();
         g.fillStyle = "aquamarine"
-        g.moveTo(252,369);
-        g.lineTo(530,434);
-        g.lineTo(525,552);
-        g.lineTo(258,486);
+        g.moveTo(25.2,36.9);
+        g.lineTo(53,43.4);
+        g.lineTo(52.5,55.2);
+        g.lineTo(25.8,48.6);
         g.closePath();
         g.stroke();
         g.fill();
+
+        g.beginPath();
+        g.fillStyle = "#6C5F42"
+        g.moveTo(66.9,56.3);
+        g.lineTo(66.7,44.2);
+        g.lineTo(73.5,40.9);
+        g.lineTo(74,52.8);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+        g.beginPath();
+        g.fillStyle = "gold"
+       
+        g.arc(72.5,47.8,0.7,0,2 * Math.PI);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+        
+
+
+        
 
 
 
